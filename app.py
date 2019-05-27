630e39f67989b586a619b165adf3869ce7356e0c
Ruben1886 <twalasik7@gmail.com>
2019-05-28 01:55:58 +0200

    fixing servers

--- app.py
+++ app.py
@@ -69,4 +69,12 @@ def game():
 
 
 if __name__ == '__main__':
-    app.run(debug = False, host = '0.0.0.0', port = 5035)
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '--heroku':
+            port = int(os.getenv('PORT'))
+            app.run(host='0.0.0.0', port=port, debug=False)
+
+        if sys.argv[1] == '--wierzba':
+            app.run(host='0.0.0.0', port=5035, debug=True)
+    else:
+        app.run(host='0.0.0.0', debug=True)
